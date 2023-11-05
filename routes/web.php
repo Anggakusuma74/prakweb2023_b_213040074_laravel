4bec47538ba8307197baee8a4b5ad5884f519962
Muhammad Angga Kusuma <kusumaangga719@gmail.com.com>
2023-11-05 15:52:21 +0700

    Vidio 4 Blade Templating Engine

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,72 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Muhammad Angga Kusuma",
         "email" => "angga.213040074@mail.unpas.ac.id",
         "image" => "Me.jpg"
     ]);
 });
 
+
+
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Muhammad Angga Kusuma",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, ipsum! Possimus minima aut facere ipsa natus voluptas neque, sint suscipit! Facere consectetur placeat, impedit earum labore quae. Sequi, asperiores dolor."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Muhammad Anggi Kusuma",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, ipsum! Possimus minima aut facere ipsa natus voluptas neque, sint suscipit! Facere consectetur placeat, impedit earum labore quae. Sequi, asperiores dolor."        
+        ]
+    ];
+
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
 });
+
+// halaman single post
+Route::get('posts/{slug}', function($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Muhammad Angga Kusuma",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, ipsum! Possimus minima aut facere ipsa natus voluptas neque, sint suscipit! Facere consectetur placeat, impedit earum labore quae. Sequi, asperiores dolor."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Muhammad Anggi Kusuma",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, ipsum! Possimus minima aut facere ipsa natus voluptas neque, sint suscipit! Facere consectetur placeat, impedit earum labore quae. Sequi, asperiores dolor."        
+        ]
+    ];
+
+    $new_post = [];
+    foreach($blog_posts as $post) {
+        if($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
+});
+
+
