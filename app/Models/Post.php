199aa725fafd40aaad70894fb96d7123ce4b086f
Muhammad Angga Kusuma <kusumaangga719@gmail.com.com>
2023-11-12 17:54:08 +0700

    Vidio 7 Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,39 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post 
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Muhammad Angga Kusuma",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, ipsum! Possimus minima aut facere ipsa natus voluptas neque, sint suscipit! Facere consectetur placeat, impedit earum labore quae. Sequi, asperiores dolor."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Muhammad Anggi Kusuma",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, ipsum! Possimus minima aut facere ipsa natus voluptas neque, sint suscipit! Facere consectetur placeat, impedit earum labore quae. Sequi, asperiores dolor."        
-        ]
-    ];
+    use HasFactory;
 
-    public static function all()
-    {
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug)
-    {
-        $posts = static::all();
-        // $post = [];
-        // foreach($posts as $p) {
-        //     if($p["slug"] === $slug) {
-        //         $post = $p;
-        //     }
-        // }
-
-        return $posts->firstWhere('slug', $slug);
-    }
+    //protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ["id"];
 }
