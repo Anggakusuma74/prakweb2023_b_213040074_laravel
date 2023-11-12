08cd02d58dd5322c5073471d6743f9427b3c781a
Muhammad Angga Kusuma <kusumaangga719@gmail.com.com>
2023-11-12 20:09:18 +0700

    Vidio 9 Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,9 +18,65 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create([
+            'name' => 'Muhammad Angga Kusuma',
+            'email' => 'angga.213040074@mail.unpas.ac.id',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Muhammad Anggi Kusuma',
+            'email' => 'anggi.213040075@mail.unpas.ac.id',
+            'password' => bcrypt('54321')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programing',
+            'slug' => 'web-programing'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim ea labore ratione rem repudiandae, numquam vitae harum, accusamus, necessitatibus modi quam impedit! Debitis hic aut, ut velit molestiae ullam doloremque mollitia harum earum amet neque et quos vero ipsam ex, fugiat quod exercitationem rerum nam optio, repudiandae minima!',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim ea labore ratione rem repudiandae, numquam vitae harum, accusamus, necessitatibus modi quam impedit! Debitis hic aut, ut velit molestiae ullam doloremque mollitia harum earum amet neque et quos vero ipsam ex, fugiat quod exercitationem rerum nam optio, repudiandae minima! Dolores labore omnis libero facere sapiente fuga quia incidunt quasi fugit praesentium error nisi, ullam harum veniam? Atque non voluptatum officiis quia nam beatae accusantium repudiandae, quibusdam deleniti unde alias. Sapiente consequatur, veritatis sequi delectus, at quis repellat dolorem eligendi accusantium odio doloremque harum, molestiae provident aperiam? Eligendi totam corporis esse minus.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim ea labore ratione rem repudiandae, numquam vitae harum, accusamus, necessitatibus modi quam impedit! Debitis hic aut, ut velit molestiae ullam doloremque mollitia harum earum amet neque et quos vero ipsam ex, fugiat quod exercitationem rerum nam optio, repudiandae minima!',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim ea labore ratione rem repudiandae, numquam vitae harum, accusamus, necessitatibus modi quam impedit! Debitis hic aut, ut velit molestiae ullam doloremque mollitia harum earum amet neque et quos vero ipsam ex, fugiat quod exercitationem rerum nam optio, repudiandae minima! Dolores labore omnis libero facere sapiente fuga quia incidunt quasi fugit praesentium error nisi, ullam harum veniam? Atque non voluptatum officiis quia nam beatae accusantium repudiandae, quibusdam deleniti unde alias. Sapiente consequatur, veritatis sequi delectus, at quis repellat dolorem eligendi accusantium odio doloremque harum, molestiae provident aperiam? Eligendi totam corporis esse minus.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim ea labore ratione rem repudiandae, numquam vitae harum, accusamus, necessitatibus modi quam impedit! Debitis hic aut, ut velit molestiae ullam doloremque mollitia harum earum amet neque et quos vero ipsam ex, fugiat quod exercitationem rerum nam optio, repudiandae minima!',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim ea labore ratione rem repudiandae, numquam vitae harum, accusamus, necessitatibus modi quam impedit! Debitis hic aut, ut velit molestiae ullam doloremque mollitia harum earum amet neque et quos vero ipsam ex, fugiat quod exercitationem rerum nam optio, repudiandae minima! Dolores labore omnis libero facere sapiente fuga quia incidunt quasi fugit praesentium error nisi, ullam harum veniam? Atque non voluptatum officiis quia nam beatae accusantium repudiandae, quibusdam deleniti unde alias. Sapiente consequatur, veritatis sequi delectus, at quis repellat dolorem eligendi accusantium odio doloremque harum, molestiae provident aperiam? Eligendi totam corporis esse minus.',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Keempat',
+            'slug' => 'judul-keempat',
+            'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim ea labore ratione rem repudiandae, numquam vitae harum, accusamus, necessitatibus modi quam impedit! Debitis hic aut, ut velit molestiae ullam doloremque mollitia harum earum amet neque et quos vero ipsam ex, fugiat quod exercitationem rerum nam optio, repudiandae minima!',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim ea labore ratione rem repudiandae, numquam vitae harum, accusamus, necessitatibus modi quam impedit! Debitis hic aut, ut velit molestiae ullam doloremque mollitia harum earum amet neque et quos vero ipsam ex, fugiat quod exercitationem rerum nam optio, repudiandae minima! Dolores labore omnis libero facere sapiente fuga quia incidunt quasi fugit praesentium error nisi, ullam harum veniam? Atque non voluptatum officiis quia nam beatae accusantium repudiandae, quibusdam deleniti unde alias. Sapiente consequatur, veritatis sequi delectus, at quis repellat dolorem eligendi accusantium odio doloremque harum, molestiae provident aperiam? Eligendi totam corporis esse minus.',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
+
     }
 }
+
+
