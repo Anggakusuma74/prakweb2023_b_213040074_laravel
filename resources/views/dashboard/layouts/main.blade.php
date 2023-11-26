ce08eb47ac2766fc12ee4a850fdfbf40deb70474
Muhammad Angga Kusuma <kusumaangga719@gmail.com.com>
2023-11-26 21:53:39 +0700

    Vidio 18 Create Post Form

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -17,6 +17,16 @@
 
 </head>
 
+{{-- Link CDN Trix Editor CSS --}}
+      <link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@2.0.0/dist/trix.css">
+
+      {{-- Menghilankan file uploud untuk Trix Editor --}}
+      <style>
+          trix-toolbar [data-trix-button-group="file-tools"] {
+              display: none;
+          }
+      </style>
+
 <body>
 
     @include('dashboard.layouts.header')
@@ -31,7 +41,8 @@
         </div>
     </div>
 
-
+    <script type="text/javascript" src="https://unpkg.com/trix@2.0.0/dist/trix.umd.min.js"></script>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
